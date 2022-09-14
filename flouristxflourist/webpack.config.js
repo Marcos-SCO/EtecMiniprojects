4df5acdd405456fe2e0554bc252456f185bfcb16
Marcos-SCO <marcos_sco@outlook.com>
2022-09-14 02:22:45 -0300

    faet: webpack initial config

--- flouristxflourist/webpack.config.js
+++ flouristxflourist/webpack.config.js
@@ -1,21 +1,30 @@
 //Webpack requires this to work with directories
 const path = require("path");
 
-const isDevelopment = process.env.NODE_ENV == 'development';
+// css extraction and minification
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const extract = require("mini-css-extract-plugin");
+// clean out build dir in-between builds
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-// This is main configuration object that tells Webpackw what to do.
+// This is main configuration object that tells Webpack what to do.
 module.exports = {
-  //default mode is production
-  mode: isDevelopment ? 'development' : 'production',
-  watch: isDevelopment,
-  //path to entry paint
-  entry: "./resources/js/index.js",
-  //path and filename of the final output
+  // target: ['web', 'es5'],
+  entry: {
+    main: [
+      path.resolve(__dirname, 'resources/js/index.js'),
+      path.resolve(__dirname, 'resources/scss/style.scss')
+    ]
+  },
+
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "script.js",
+    filename: "js/[name].js",
+    iife: false, 
+    environment: {
+      arrowFunction: false,
+    },
   },
 
   module: {
@@ -31,40 +40,40 @@ module.exports = {
         },
       },
       {
-        test: /\.(sa|sc|c)ss$/,
+        test: /\.(sa|sc|c)ss$/i,
         use: [
-          {
-            loader: extract.loader,
-          },
-          {
-            loader: "css-loader",
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              implementation: require("sass"),
-            },
-          },
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
       },
       {
-        //applying rule
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [
-          {
-            //using file-loader
-            loader: "file-loader",
-            options: {
-              outputPath: "img",
-            },
-          },
-        ],
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
       },
     ],
   },
+
   plugins: [
-    new extract({
-      filename: "style.css",
+    // clear out build directories on each build
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        'build/js/*',
+        'build/css/*'
+      ]
+    }),
+
+    // css extraction into dedicated file
+    new MiniCssExtractPlugin({
+      filename: './css/style.css'
     }),
   ],
+
+  optimization: {
+    // minification - only performed when mode = production
+    minimizer: [
+      // css minification
+      new CssMinimizerPlugin(),
+    ]
+  },
 };
